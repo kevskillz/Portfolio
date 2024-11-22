67389aa266e1ed87b64e43e1767d153328ccde8d
kevskillz <kevskillz10@gmail.com>
2024-11-22 14:50:33 -0500

    s

--- configs/header-nav-links.ts
+++ configs/header-nav-links.ts
@@ -2,5 +2,4 @@ export const headerNavLinks = [
     { href: '/blog', title: 'Blog' },
     { href: '/blog/tags', title: 'Tags' },
     { href: '/blog/projects', title: 'Projects' },
-    { href: 'https://notes.alexpavlov.dev', title: 'My notes' }
 ]
