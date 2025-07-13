16ac347b350ef314c31871c551c035fb077406c9
Kevin Lobo <66384762+kevskillz@users.noreply.github.com>
2025-07-13 10:06:42 -0700

    Update header-nav-links.ts

--- configs/header-nav-links.ts
+++ configs/header-nav-links.ts
@@ -2,6 +2,6 @@ export const headerNavLinks = [
     { href: '/blog', title: 'Blog' },
     { href: '/blog/tags', title: 'Tags' },
     { href: '/blog/projects', title: 'Projects' },
-    { href: 'https://drive.google.com/file/d/1mDqZb7eBd3eOsRew39y20QxMNgtvRtjz/view?usp=sharing', title: 'Resume' },
+    { href: 'https://drive.google.com/file/d/1nJhMm911_LJ9LBTxQD8LIXICs7OdY7bG/view?usp=sharing', title: 'Resume' },
 
 ]
