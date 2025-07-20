644a75c3fd31961b5ff21d09887c6cdaa3000bfa
Kevin Lobo <66384762+kevskillz@users.noreply.github.com>
2025-07-20 11:22:57 -0700

    Update intro-links.tsx

--- app/components/intro-links.tsx
+++ app/components/intro-links.tsx
@@ -65,7 +65,7 @@ export function IntroLinks() {
         <div className="my-2 grid items-start gap-8">
           <div className="group relative">
             <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r from-fuchsia-600 to-emerald-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-            <Link href="https://drive.google.com/file/d/1nJhMm911_LJ9LBTxQD8LIXICs7OdY7bG/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <Link href="https://drive.google.com/file/d/1nAbJ8Hzp3ib3n2MrWNATgSeXhQJggd9c/view?usp=sharing" target="_blank" rel="noopener noreferrer">
               <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
                 <span className="flex items-center space-x-5">
                   <svg
